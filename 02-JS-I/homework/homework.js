4080e7b68777c15602b2f1326c653c1ce69a011d
unknown <marinellicarolinadaiana@gmail.com>
2021-10-28 01:08:03 -0300

    Homework1 COMPLETADO

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -162,7 +162,7 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-return nombre + "" + apellido
+return nombre + " " + apellido ;
 
 }
 
@@ -171,7 +171,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return "Hola" + "nombre" + "!" ;
+  return "Hola"+ " " + nombre + "!" ;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
@@ -199,7 +199,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  return euro * 1,20
+  return euro * 1.20;
 }
 
 
@@ -209,7 +209,9 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-    if (letra.length > 1) return "Dato incorrecto" ; if (letra === a || e || i || o || u ) return "Es vocal" ; else return "Dato incorrecto"
+  if (letra.length > 1) return "Dato incorrecto"; 
+  if (letra !== "a"||"e"||"i"||"o"||"u") return "Dato incorrecto"; 
+  if (letra === "a"||"e"||"i"||"o"||"u") return "Es vocal";
 }
 
 
