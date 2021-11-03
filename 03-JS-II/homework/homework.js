70b2afa638826d1919028dfc046e3e151effe0d4
unknown <marinellicarolinadaiana@gmail.com>
2021-11-02 23:05:48 -0300

    FALTAN RESOLVER 4 ERRORES

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -98,9 +98,9 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-  if (numero % 3=== 0) return "fizz";
-  if (numero% 5=== 0) return "buzz";
-  if (numero % 3 ===0 && numero % 5 === 0) return "fizzbuzz";
+  if (numero % 15=== 0) return "fizzbuzz";
+  if (numero% 3=== 0) return "fizz";
+  if (numero% 5 === 0) return "buzz";
    else return numero;
 }
 
@@ -122,11 +122,11 @@ function esPrimo(numero) {
   // De lo contrario devuelve "falso"
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
-  // Nota: Los números 0 y 1 NO son considerados números primos
-   if( numero < 2) return false;
-   if(numero === 2) return true;
-   for(var i = 2; i < numero; i++)
-   if(numero % i === 0) return false;
+  // Nota: Los números 0 y 1 NO son considerados números
+  if (numero === 2) return true;
+  if (numero < 2) return false;
+  for(var i = 2; i < numero; i++)
+  if(numero % i === 0) return false; else return true;
   
 }
 
